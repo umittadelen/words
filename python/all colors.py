f009c346f849120acbac0704a075b08aaeb1ee25
umittadelen <72702832+umit-09@users.noreply.github.com>
2023-02-17 09:56:59 +0100

    Update all colors.py

--- python/all colors.py
+++ python/all colors.py
@@ -6,14 +6,15 @@ width, height = 40960, 40960
 # Create a new transparent image
 img = Image.new("RGBA", (width, height), (0, 0, 0, 0))
 
-# Iterate through all possible RGBA color tones
-for r in range(256):
-  for g in range(256):
-    for b in range(256):
-      for a in range(256):
-        # Set the pixel color with the given transparency
-        print(f"r:{r} g:{g} b:{b} a:{a}")
-        img.putpixel((r, g), (r, g, b, int(255 * (a / 100))))
+# Iterate through all possible RGBA color tones and alpha values
+for x in range(width):
+  for y in range(height):
+    for r in range(256):
+      for g in range(256):
+        for b in range(256):
+          for a in range(256):
+            # Set the pixel color with the given transparency
+            img.putpixel((x, y), (r, g, b, a))
 
 # Save the image to the desktop
 img.save("../rgba_colors.png")
